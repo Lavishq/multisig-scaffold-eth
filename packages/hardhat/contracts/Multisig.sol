51d13cc7bc56add7b625598baa3da69551b6b9d2
Lavishq <v.whitehearted@simplelogin.co>
2023-01-15 12:50:24 +0530

    multisig can trasfer erc20 or nft or any function using the calldata

--- packages/hardhat/contracts/Multisig.sol
+++ packages/hardhat/contracts/Multisig.sol
@@ -1,46 +1,56 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.17;
 
 contract MultiSig {
     address[] public owners;
-    uint256 public required; 
+    uint256 public required;
     uint256 public transactionCount;
 
     struct Transaction {
         address destination;
         uint256 tx_value;
         bool executed;
+        bytes data;
     }
 
     mapping(uint256 => Transaction) public transactions;
-    mapping(uint256 => mapping(address=> bool)) public confirmations;
+    mapping(uint256 => mapping(address => bool)) public confirmations;
 
-    constructor (address[] memory _owners, uint _required) {
+    constructor(address[] memory _owners, uint _required) {
         require(_required > 0);
-        require(_owners.length>_required);
+        require(_owners.length > _required);
         required = _required;
-        owners=_owners;
+        owners = _owners;
     }
 
     receive() external payable {}
 
-    function addTransaction(address _destination, uint256 _value) internal returns(uint) {
+    function addTransaction(
+        address _destination,
+        uint256 _value,
+        bytes memory _data
+    ) internal returns (uint) {
         uint currentTxCount = transactionCount;
-        transactions[transactionCount] = Transaction(_destination, _value, false);
+        transactions[transactionCount] = Transaction(
+            _destination,
+            _value,
+            false,
+            _data
+        );
         transactionCount += 1;
         return currentTxCount;
     }
 
     function confirmTransaction(uint _txId) public {
         bool isOwner;
-        for(uint i =0; i< owners.length;i++) {
-            if(owners[i] == msg.sender){
-                isOwner =true;
+        for (uint i = 0; i < owners.length; i++) {
+            if (owners[i] == msg.sender) {
+                isOwner = true;
             }
         }
         require(isOwner);
         confirmations[_txId][msg.sender] = true;
-        if(isConfirmed(_txId)) {
+        if (isConfirmed(_txId)) {
             executeTransaction(_txId);
         }
     }
@@ -48,7 +58,7 @@ contract MultiSig {
     function getConfirmationsCount(uint _txId) public view returns (uint) {
         address[] memory _owners = owners;
         uint total_confirmations;
-        for (uint i = 0; i< _owners.length; i++) {
+        for (uint i = 0; i < _owners.length; i++) {
             if (confirmations[_txId][_owners[i]] == true) {
                 total_confirmations++;
             }
@@ -56,8 +66,12 @@ contract MultiSig {
         return total_confirmations;
     }
 
-    function submitTransaction(address _destination, uint256 _value) public {
-        uint currentTxId = addTransaction(_destination, _value);
+    function submitTransaction(
+        address _destination,
+        uint256 _value,
+        bytes memory _data
+    ) public {
+        uint currentTxId = addTransaction(_destination, _value, _data);
         confirmTransaction(currentTxId);
     }
 
@@ -68,7 +82,9 @@ contract MultiSig {
     function executeTransaction(uint _txId) public {
         require(isConfirmed(_txId));
         Transaction storage currTx = transactions[_txId];
-        (bool sent,) = currTx.destination.call{value: currTx.tx_value}("");
+        (bool sent, ) = currTx.destination.call{value: currTx.tx_value}(
+            currTx.data
+        );
         require(sent);
         currTx.executed = true;
     }
